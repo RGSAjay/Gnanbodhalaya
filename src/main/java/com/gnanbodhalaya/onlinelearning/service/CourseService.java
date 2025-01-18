4c1c4d601545b772a2b7a513c47bcafb0f83dfcf
RAVULAPALLI GURU SANKAR AJAY <ajayrgs123@gmail.com>
2025-01-19 00:03:23 +0530

    Add service/CourseService.java file

---
+++ src/main/java/com/gnanbodhalaya/onlinelearning/service/CourseService.java
@@ -0,0 +1,37 @@
+package com.gnanbodhalaya.onlinelearning.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gnanbodhalaya.onlinelearning.model.Course;
+import com.gnanbodhalaya.onlinelearning.repository.CourseRepository;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    public Course save(Course course) {
+        return courseRepository.save(course);
+    }
+
+    public Optional<Course> getCourseById(Long id) {
+        return courseRepository.findById(id);
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    public void deleteCourse(Long id) {
+        courseRepository.deleteById(id);
+    }
+
+    public Course updateCourse(Course course) {
+        return courseRepository.save(course);
+    }
+}
